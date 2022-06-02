ef211a6a230608cd0069f676ba494c7e4cf937b5
박지수 <jisupark2000@gmail.com>
2022-06-03 00:24:06 +0900

    complete bubbleSort

---
+++ sorts/bubbleSort.py
@@ -0,0 +1,6 @@
+def bubbleSort(lst):
+    for i in range(len(lst), 0, -1):
+        for j in range(i - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    return lst
